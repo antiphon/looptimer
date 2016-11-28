8891f1898d7ebdd7e7090de658e1a3e3791452ad
Tuomas Rajala <tuomas.rajala@iki.fi>
2016-11-28 19:07:11 +0000

    improved print method, adaptive mean.

--- R/looptimer.R
+++ R/looptimer.R
@@ -6,7 +6,7 @@
 #' @param n total count in the loop
 #' @param i iteration that was just finished
 #' @param when_ready if true add ETA to message.
-#' 
+#' @param memory How many iteration back remember when computing the average time
 #' @details
 #' Approximative iteration speed, time left, and ETA.
 #' 
@@ -18,22 +18,28 @@
 #' for(i in 1:N){
 #'  # do something
 #'  Sys.sleep(runif(1))
-#'  t0 <- looptimer(t0, N, i, TRUE)
-#'  cat(t0$message,"\n")
+#'  print(  t0 <- looptimer(t0, N, i, TRUE) )
 #' }
 #' 
 #' 
 #' @export
-looptimer <- function(tim, n, i, when_ready=TRUE){
+looptimer <- function(tim, n, i, when_ready=TRUE, memory=50){
   if(missing(tim)){
     tim <- list(tvec=NULL, speed=Inf)
-  }else{
+    if(!missing(n)) {tim$n <- n; tim$i <- 0}
+  }
+  else{
     ti <- difftime(Sys.time(), tim$Tlast, units="secs")
     tim$tvec <- c(tim$tvec, ti)
-    tim$speed <- mean(tim$tvec, trim = 0.1) ## speed in secs
+    ndata <- length(tim$tvec)
+    tim$speed <- mean(tim$tvec[ndata+1-1:min(ndata, memory)], trim = 0.1) ## speed in secs
     tim$eta <- NA
     tim$message <- NULL
-    if(!missing(n)&!missing(i)) {
+    if(missing(n)){ n <- NULL; if(!is.null(tim$n)) n <- tim$n }
+    if(missing(i)){ i <- NULL; if(!is.null(tim$i)) i <- (tim$i <- tim$i + 1 )}
+    if(!missing(n)) tim$n <- n
+    if(!missing(i)) tim$i <- i
+    if(!is.null(n) & !is.null(i)){
       tim$eta <- Sys.time()+tim$speed*(n-i)
       tim$left <- format(tim$eta-Sys.time(), digits = 2)
       end <- if(when_ready) paste0(", ready ", format(tim$eta),"]") else "]"
